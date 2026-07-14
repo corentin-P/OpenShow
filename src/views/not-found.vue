38b172d9e564c47b947c44241065d0df623911f3
Corentin <70685440+corentin-P@users.noreply.github.com>
2026-07-14 21:18:16 +0200

    fix: router bug + not found page (#22)

---
+++ src/views/not-found.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="not-found">
+    <h1>{{ $t('not-found.title') }}</h1>
+    <router-link class="button button-arrow" to="/"> {{ $t('not-found.home') }} </router-link>
+  </div>
+</template>
+
+<style lang="css">
+    @import '@/assets/styles/components/buttons.css';
+    .not-found {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 60vh;
+        text-align: center;
+    }
+</style>
